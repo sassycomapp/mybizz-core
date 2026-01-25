72faa1c2c0824c52ef40ba2cd17f5f0318b9252b
sassycomapp@gmail.com <sassycomapp@gmail.com>
2026-01-25 14:57:38 +0000

    Edited Form2

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -16,10 +16,10 @@ class Form2(Form2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    @handle("outlined_button_1", "click")
+
     # Any code you write here will run before the form opens.
 
-
+  @handle("outlined_button_1", "click")
   def outlined_button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     pass
