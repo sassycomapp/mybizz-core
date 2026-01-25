8f2b4df40cb374475a9ab6dfc14a90d33eda599b
sassycomapp@gmail.com <sassycomapp@gmail.com>
2026-01-25 14:41:23 +0000

    Created Form2

---
+++ client_code/Form2/__init__.py
@@ -0,0 +1,25 @@
+from ._anvil_designer import Form2Template
+from anvil import *
+import anvil.server
+import m3.components as m3
+from routing import router
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import stripe.checkout
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class Form2(Form2Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    @handle("outlined_button_1", "click")
+    # Any code you write here will run before the form opens.
+
+
+  def outlined_button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
